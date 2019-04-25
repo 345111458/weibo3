a53515b8164cdf57af9d8cf2c5d00e05f8a4420b
Your Name <345111458@qq.com>
2019-04-25 09:21:50 +0000

    用户显示页面

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,8 @@ class User extends Authenticatable
 
     protected $table = 'users';
 
+
+
     /**
      * The attributes that are mass assignable.
      *
@@ -38,4 +40,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    //Gravatar 头像和侧边栏
+    public function gravatar($size = '100'){
+
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
 }
