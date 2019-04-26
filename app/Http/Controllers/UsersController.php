fd1cb2955b41fe950a5b314b7f2a422a6ed2faa2
Your Name <345111458@qq.com>
2019-04-26 10:10:47 +0000

    用户微博列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -37,9 +37,10 @@ class UsersController extends Controller{
 
 
     public function show(User $user){
+        $statuses = $user->statuses()->orderBy('created_at','desc')->paginate(10);
 
 
-        return view('users.show',compact('user'));
+        return view('users.show',compact('user','statuses'));
     }
 
 
